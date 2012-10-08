42a530667097104eb72592b11330e50d9d647e57
Evan Hazlett <ejhazlett@gmail.com>
2012-10-08 00:25:00 -0400

    added support for configuring rqscheduler via environment variables

--- rq_scheduler/scripts/rqscheduler.py
+++ rq_scheduler/scripts/rqscheduler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 from redis import Redis
 from rq_scheduler.scheduler import Scheduler
@@ -8,10 +9,14 @@ from rq_scheduler.scheduler import Scheduler
 
 def main():
     parser = argparse.ArgumentParser(description='Runs RQ scheduler')
-    parser.add_argument('-H', '--host', default='localhost', help="Redis host")
-    parser.add_argument('-p', '--port', default=6379, type=int, help="Redis port number")
-    parser.add_argument('-d', '--db', default=0, type=int, help="Redis database")
-    parser.add_argument('-P', '--password', default=None, help="Redis password")
+    parser.add_argument('-H', '--host', default=os.environ.get('REDIS_HOST',
+        'localhost'), help="Redis host")
+    parser.add_argument('-p', '--port', default=os.environ.get('REDIS_PORT', 6379),
+        type=int, help="Redis port number")
+    parser.add_argument('-d', '--db', default=os.environ.get('REDIS_DB', 0),
+        type=int, help="Redis database")
+    parser.add_argument('-P', '--password', default=os.environ.get('REDIS_PASSWORD',
+        None), help="Redis password")
     parser.add_argument('-i', '--interval', default=60, type=int
         , help="How often the scheduler checks for new jobs to add to the \
             queue (in seconds).")
