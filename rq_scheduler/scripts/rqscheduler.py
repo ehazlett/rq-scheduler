086a39d155d2c57942fa74fb9dd29c57c65d2ae7
Evan Hazlett <ejhazlett@gmail.com>
2012-10-08 00:50:41 -0400

    added support for configuring rqscheduler from config file

--- rq_scheduler/scripts/rqscheduler.py
+++ rq_scheduler/scripts/rqscheduler.py
@@ -15,7 +15,19 @@ def main():
     parser.add_argument('-i', '--interval', default=60, type=int
         , help="How often the scheduler checks for new jobs to add to the \
             queue (in seconds).")
+    parser.add_argument('-c', '--config', default=None, help="Path to config \
+        file")
     args = parser.parse_args()
+    if args.config: # load from config
+        import imp
+        settings = imp.load_source('settings', args.config)
+        cfg = {
+            'host': getattr(settings, 'REDIS_HOST'),
+            'port': getattr(settings, 'REDIS_PORT'),
+            'db': getattr(settings, 'REDIS_DB'),
+            'password': getattr(settings, 'REDIS_PASSWORD'),
+        }
+        args.__dict__.update(cfg)
     connection = Redis(args.host, args.port, args.db, args.password)
     scheduler = Scheduler(connection=connection, interval=args.interval)
     scheduler.run()
